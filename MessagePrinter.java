24fbe67516e1437d84249ea1be889b89bfe98f27
Alina <alina.khizhnyak.2000@gmail.com>
2025-11-26 16:52:24 +0200

    Add time message functionality

--- MessagePrinter.java
+++ MessagePrinter.java
@@ -5,9 +5,10 @@ return "Hello! Welcome to Git and GitHub Tutorial! Learning is fun!";
 }
  main
 
-//    public static String getWelcomeMessage() {
-//        return "Welcome to Git and GitHub!";
-//    }
+    public static String getTimeMessage() {
+        return "Current time: " + java.time.LocalTime.now();
+    }
+
 
     public static String getDateMessage() {
         return "Today is: " + java.time.LocalDate.now();
@@ -21,7 +22,9 @@ return "Hello! Welcome to Git and GitHub Tutorial! Learning is fun!";
     public static void main(String[] args) {
         System.out.println(getWelcomeMessage());
         System.out.println(getDateMessage());
+        System.out.println(getTimeMessage());
         System.out.println("Learning remote repositories!");
         System.out.println(getGoodbyeMessage());
     }
+
 }
